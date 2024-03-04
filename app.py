e9a1f0ed44cd6eb20c98c5298911936c8e35019c
zll <jinmi45678@163.com>
2024-03-04 14:34:34 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -67,10 +67,18 @@ class Movie(db.Model):  # 表名将会是 movie
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
 
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    user = User.query.first()   # 从数据库查询并获取第一条记录，将结果存储在变量user中
+    return render_template('404.html'), 404  # 返回模板和状态码
 
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}，可在模板中直接使用user变量
 
 @app.route('/')
 def index():
     user = User.query.first()  # 从数据库中读取用户记录
     movies = Movie.query.all()  # 从数据库中读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
